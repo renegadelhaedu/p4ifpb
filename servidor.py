3afc435944efe3ef92939bd228fb453cbd87e9e5
renegadelha <renegadelha@gmail.com>
2024-10-21 15:38:00 -0300

    aula2110

--- servidor.py
+++ servidor.py
@@ -1,12 +1,24 @@
 #importando o flask
 from flask import *
 
+convidados = ['jose','maria','pedro','ana','thais','carla','joaquim']
+
 #criando o servidor flask (back-end)
 app = Flask(__name__)
 
 @app.route('/')
 def pageprincipal():
     return render_template('homepage.html')
+
+@app.route('/verificarconvite', methods =['POST'])
+def verificar_convite():
+    #receber um dado vindo de um formulário
+    nome = request.form.get('nomeuser')
+    if nome in convidados:
+        return render_template('sim.html')
+    else:
+        return render_template('nao.html')
+
 
 #iniciando/rodando o servidor
 app.run()
