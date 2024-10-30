8eddba40c0737f3b3718fd6d36a3e981a7f6972f
renegadelha <renegadelha@gmail.com>
2024-10-30 17:20:00 -0300

    aula3010

--- servidor.py
+++ servidor.py
@@ -1,24 +1,39 @@
 #importando o flask
 from flask import *
 
+usuarios = [['rene','1234'],['maria','6543']]
 
 #criando o servidor flask (back-end)
 app = Flask(__name__)
 
 @app.route('/')
 def pageprincipal():
-    return render_template('thaynaraifpb.html')
+    return render_template('homeifpb.html')
 
 
-@app.route('/inserir')
+@app.route('/inseriraluno')
 def inserir_user():
-    login = request.form.get('')
+    login = request.form.get('login')
+    senha = request.form.get('senha1')
+    return "inserir aluno"
 
-@app.route('/login')
+
+@app.route('/login', methods=['POST'])
 def login():
-    login = request.form.get('senha')
-    senha = request.form.get('login')
+    login = request.form.get('login')
+    senha = request.form.get('senha')
+    logado = False
 
+    for user in usuarios:
+        if login == user[0] and senha == user[1]:
+            logado = True
+            break
+
+    if logado:
+        return render_template('homepagealuno.html', user=login)
+    else:
+        msg='Senha ou login incorretos'
+        return render_template('homeifpb.html', msglogin=msg)
 
 #iniciando/rodando o servidor
 app.run(debug=True)
