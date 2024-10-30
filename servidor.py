63f040d955ed5950c30b5c2592b9742dbe2f8300
renegadelha <renegadelha@gmail.com>
2024-10-30 13:51:30 -0300

    aula3010 foto ifpb

--- servidor.py
+++ servidor.py
@@ -1,7 +1,6 @@
 #importando o flask
 from flask import *
 
-usuarios = []
 
 #criando o servidor flask (back-end)
 app = Flask(__name__)
@@ -19,16 +18,6 @@ def inserir_user():
 def login():
     login = request.form.get('senha')
     senha = request.form.get('login')
-
-@app.route('/verificarconvite', methods =['POST'])
-def verificar_convite():
-    #receber um dado vindo de um formulário
-    nome = request.form.get('nomeuser')
-    idade = int(request.form.get('idadeuser'))
-    if nome in convidados and idade >= 18:
-        return render_template('sim.html')
-    else:
-        return render_template('nao.html')
 
 
 #iniciando/rodando o servidor
